357436e885d60373c75c553b351c09be844897f7
Daniel Draper <daniel@codefire.com.au>
2010-08-10 22:27:28 +0930

    Basic gift CrUD (refs #5003)

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,3 +1,14 @@
 class Gift < ActiveRecord::Base
   has_and_belongs_to_many :offers
+  has_attached_file :gift_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_presence_of :name, :description, :on_hand
+  validates_uniqueness_of :name
+  validates_numericality_of :on_hand
+
+  default_scope :order => :name
+
+  after_destroy do |gift|
+    # TODO: Admin Audit Log
+    # TODO: Remove gift from all offers (or does this happen anyway?)
+  end
 end
--- db/migrate/20100805131457_create_gifts.rb
+++ db/migrate/20100805131457_create_gifts.rb
@@ -3,10 +3,10 @@ class CreateGifts < ActiveRecord::Migration
     create_table :gifts do |t|
       t.string :name
       t.text :description
-      t.string :image_file_name
-      t.string :image_content_type
-      t.integer :image_file_size
-      t.timestamp :image_updated_at
+      t.string :gift_image_file_name
+      t.string :gift_image_content_type
+      t.integer :gift_image_file_size
+      t.timestamp :gift_image_updated_at
       t.integer :on_hand
       t.timestamps
     end
