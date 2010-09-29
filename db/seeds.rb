47dd870ea9abfad282d3d5ea477034d77213b9d2
shamim jahandideh <shamim@shamim-jahandidehs-iMac.local>
2010-09-29 17:27:05 +0930

    master- adding seed data for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+#FIXME
+User.create!(:login => "netfox",
+            :firstname => "netfox", 
+            :lastname => "netfox", 
+            :email => "admin@crikey.netfox.com", 
+            :email_confirmation => "admin@crikey.netfox.com", 
+            :phone_number => "netfox",
+            :address_1 => "netfox",
+            :city => "adelaide",
+            :postcode => "5000",
+            :state => "SA",
+            :country => "Australia",
+            :role => "admin",
+            :password => "netfox",
+            :password_confirmation => "netfox")
