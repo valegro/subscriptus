0e8a8ccffe3b04fe729cab35ae550db0bdbbf4f5
Daniel Draper <daniel@codefire.com.au>
2010-10-07 15:41:05 +1030

    Added production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,16 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+# Setup Active Merchant for development
+config.after_initialize do
+	ActiveMerchant::Billing::Base.mode = :test
+  # Eway Gateway Settings
+  ::GATEWAY = ActiveMerchant::Billing::SecurePayAuExtendedGateway.new(  # the default_currency of this gateway is 'AUD'
+        :login => 'CKR0030',  # <MerchantID> input to Au securePay Gateway.
+        :password => "jogkriw7"
+  )
+end
+CAMPAIGNMASTER_USERNAME = 'ddraper'
+CAMPAIGNMASTER_PASSWORD = 'netfox'
+CAMPAIGNMASTER_CLIENT_ID = '5032'
