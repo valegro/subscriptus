124b96c33faf37bcecb7e1520ee62d5d4a9f76fa
Daniel Draper <daniel@codefire.com.au>
2010-10-14 10:30:37 +1030

    Handled rare (impossible?) case of nil offer

--- app/helpers/subscribe_helper.rb
+++ app/helpers/subscribe_helper.rb
@@ -28,6 +28,7 @@ module SubscribeHelper
   end
 
   def describe_offer_term(ot)
+    return "" unless ot
     if ot.expires?
       "#{pluralize(ot, 'month')} for #{number_to_currency_with_free(ot.price)}"
     else
