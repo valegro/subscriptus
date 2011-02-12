d438b98e6ac52c4e1bdedf907d1fca4dc9261b68
Daniel Draper <daniel@codefire.com.au>
2011-02-12 16:17:05 +1030

    Change database settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ task :to_production do
   set :user, "root"
   set :password, "zxnm9014"
   set :prod_db, "subscriptus"
-  set :db_hostname, "zebra.crikey.com.au"
+  set :db_hostname, "localhost"
   set :db_port, 3306
 
   role :app, "deploy@zebra.crikey.com.au"
