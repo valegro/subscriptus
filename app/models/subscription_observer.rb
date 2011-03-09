69d4a2c5a7dd91a1cd87e346474d46e1401abc8e
Warren Seen <warren@warrenseen.com>
2011-03-09 23:51:14 +1100

    Add subscription notification triggers. Still needs email copy. [Refs #4211182, #4211180, #4211177]

--- app/models/subscription_observer.rb
+++ app/models/subscription_observer.rb
@@ -1,6 +1,16 @@
 class SubscriptionObserver < ActiveRecord::Observer
 
-
+  def after_enter_pending(subscription)
+    SubscriptionMailer.deliver_pending(subscription)
+  end
+  
+  def after_exit_pending(subscription)
+    case subscription.state
+    when 'active' then SubscriptionMailer.deliver_verified(subscription)
+    when 'squatter' then SubscriptionMailer.deliver_pending_expired(subscription)
+    end
+  end
+  
 
   def before_enter_active(subscription)
     # TODO: Set expires_at here
--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  
+  NO_REPLY = "noreply@crikey.com.au"
   def new_user(user)
     recipients user.email
     subject "New Crikey User"
-    from "noreply@crikey.com.au"
+    from NO_REPLY
     body :user => user
     content_type 'text/html'
   end
