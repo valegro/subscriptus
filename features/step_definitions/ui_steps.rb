315fccc79057b8d34a89af0fb00015d8fa46a583
Daniel Draper <daniel@codefire.com.au>
2011-04-06 10:01:36 +0930

    Fixed a broken cuke

--- features/step_definitions/ui_steps.rb
+++ features/step_definitions/ui_steps.rb
@@ -1,3 +1,9 @@
+When /^I follow "([^"]*)" and click OK$/ do |text|
+  page.evaluate_script("window.alert = function(msg) { return true; }")
+  page.evaluate_script("window.confirm = function(msg) { return true; }")
+  When %{I follow "#{text}"}
+end
+
 Then /^I should see that "([^"]*)"(?: within "([^"]*)")? is invisible$/ do |elem, selector|
   with_scope(selector) do
     page.should have_no_xpath('//*', :text => elem, :visible => false)
