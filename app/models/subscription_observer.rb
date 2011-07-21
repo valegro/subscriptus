89a7caa15fff6fea1319a1699217cb921c2ab317
Warren Seen <warren@warrenseen.com>
2011-07-21 14:31:19 +1000

    Make sure state callbacks are processed explicitly

--- app/models/subscription_observer.rb
+++ app/models/subscription_observer.rb
@@ -26,8 +26,16 @@ class SubscriptionObserver < ActiveRecord::Observer
   end
   
   def after_save(subscription)
+    # This is copied from StateCallbacks 
+    # overriding a method like after_save in a module that isn't immediately obvious to other 
+    # devs using the callbacks is risky.
+    state_changes = subscription.changes['#{state_name}']
+    from, to = state_changes.try(:first), state_changes.try(:last)
+    self.class.state_change_callback(from, to, subscription)
+    
     if subscription.state_changed?
       if user = subscription.try(:user)
+        Rails.logger.info "Scheduling wordpress sync for user: #{user.inspect}"
         user.delay.sync_to_wordpress
         # user.sync_to_wordpress
       end
