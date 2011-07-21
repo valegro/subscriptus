2d5a9a50f8db8521532bb880e111687289e6e8c3
Warren Seen <warren@warrenseen.com>
2011-07-21 11:53:57 +1000

    Fix suspend/unsuspend links in subscriber page for admin.

--- app/helpers/admin/subscriptions_helper.rb
+++ app/helpers/admin/subscriptions_helper.rb
@@ -15,8 +15,17 @@ module Admin::SubscriptionsHelper
 
   def subscription_event_links(subscription)
     # Don't allow manual expiration
-    subscription.allowed_events_for(:state).select { |event| ![ :cancel, :pay_later, :expire, :enqueue_for_renewal, :postpone, :new_trial ].include?(event.to_sym) }.map do |event|
+    links = subscription.allowed_events_for(:state).select { |event| ![ :cancel, :pay_later, :expire, :enqueue_for_renewal, :postpone, :new_trial, :suspend, :unsuspend ].include?(event.to_sym) }.map do |event|
       link_to(event.humanize, send("#{event}_admin_subscription_path", subscription), :confirm => "Are you sure you want to #{event} this subscription?")
+    end
+    
+    links.tap do |l|
+      l << case
+      when subscription.active?  && subscription.expires_at
+        link_to_remote 'Suspend', :url => suspend_admin_subscription_path(subscription), :method => :get
+      when subscription.suspended?
+        link_to 'Unsuspend', unsuspend_admin_subscription_path(subscription), :method => :post
+      end
     end
   end
 end
